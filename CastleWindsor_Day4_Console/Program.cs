4f0a04c6cee9b84ce45a9a7d92a89e8bb91678c1
phatlino <phatlongtoan@gmail.com>
2022-04-23 09:40:31 +0700

    Add project files.

---
+++ CastleWindsor_Day4_Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using CastleWindsor_Day4_Console.Utilities;
+using CastleWindsor_Day4_Console.Data;
+using CastleWindsor_Day4_Console.BusinessLogic;
+using Castle.Windsor;
+using Castle.MicroKernel.Registration;
+using CastleWindsor_Day4_Console.Data.InterFace;
+
+namespace CastleWindsor_Day4_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //format encoding 
+            Console.OutputEncoding = Encoding.UTF8;
+            //Process     
+
+            //Constructor Injection - sử dụng nguyên tắc DI (Dependency Injection)
+            //SVBusinessLogic svBL = new SVBusinessLogic(new DataAccessList());
+
+            //sử dụng thư viện Castle Windsor
+            try
+            {
+                WindsorContainer container = new WindsorContainer();
+                container.Register(Component.For<ISinhVienDAL>().ImplementedBy<DataAccessList>(),
+                                   Component.For<SVBusinessLogic>());
+                Execute.RunMain(container);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
